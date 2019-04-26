fb1aa88ef7d17bcbd06a58fc2a0268309d661500
Lyq001 <13761753949@163.com>
2019-04-26 11:20:49 +0800

    提交源码文件

---
+++ src/com/lyq/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.lyq.dao;
+
+import com.lyq.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
